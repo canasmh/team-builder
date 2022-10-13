40da463adf75724d1b5d671c17d0228145971b2b
canasmh <canasmh@yahoo.com>
2022-10-13 14:21:35 -0400

    Create fake data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 0,
+        name: "Manuel Canas",
+        email: "canasmh@yahoo.com",
+        role: "Full Stack Developer"
+    },
+    {
+        id: 1,
+        name: "Carlos Lopez",
+        email: "some@email.com",
+        role: "Frontend Developer"
+    },
+    {
+        id: 2,
+        name: "John Smith",
+        email: "johnsmith@gmail.com",
+        role: "Backend Developer"
+    },
+    {
+        id: 3,
+        name: "Jane Doe",
+        email: "janedoe@gmail.com",
+        role: "Designer"
+    }
+];
+
+export default data;
